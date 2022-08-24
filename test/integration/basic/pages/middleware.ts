8c2155f27365e37e72f7b713898da32bae5413b6
zhuhaow <zhuhaow@gmail.com>
2022-08-24 21:24:53 +0800

    Work around bug https://github.com/vercel/next.js/issues/39800

--- test/integration/basic/pages/middleware.ts
+++ test/integration/basic/pages/middleware.ts
@@ -3,3 +3,7 @@ import {NextRequest, NextResponse} from 'next/server';
 export function middleware(request: NextRequest) {
 	return NextResponse.next(request);
 }
+
+export const config = {
+	runtime: 'experimental-edge',
+};
